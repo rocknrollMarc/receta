065ec394afae675c4530bd0b9d9f4b03feb4808a
Marc Blümner <m.bluemner@liquidlabs.de>
2014-08-05 12:50:01 +0200

    first feature test spec

---
+++ spec/features/search_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+feature "Looking up recipies", js: true do
+  scenario "finding recipies" do
+    visit '/'
+    fill_in "keywords", with: "baked"
+    click_on "Search"
+
+    expect(page).to have_content("Baked Potato")
+    expect(page).to have_content("Baked Brussel Sprouts")
+  end
+end
