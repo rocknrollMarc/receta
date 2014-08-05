6db980b1e8f5db5f6a938cd256f96d461a47d5d2
Marc Blümner <m.bluemner@liquidlabs.de>
2014-08-05 13:24:48 +0200

    add index recipe controller

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -1,4 +1,9 @@
 class RecipiesController < ApplicationController
   def index
+    @recipes = if params[:keywords]
+                 Recipe.where('name ilike ?', "%#{params[:keywords]}%")
+               else
+                 []
+               end
   end
 end
--- spec/controllers/recipies_controller_spec.rb
+++ spec/controllers/recipies_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe RecipesController do
+describe "RecipesController" do
   render_views
   describe "index" do
     before do
